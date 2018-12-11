30bd250f7292b560225233848cc3732eec438e1c
chingchun0531 <chingchunlin890531@gmail.com>
2018-12-11 12:01:58 +0800

    unfinished job2

---
+++ src/com/exam/Order.java
@@ -0,0 +1,13 @@
+package com.exam;
+
+import java.util.ArrayList;
+
+public class Order {
+	ArrayList bag = new ArrayList();
+
+	public Order() {
+
+	}
+
+
+}
---
+++ src/com/exam/Ticket.java
@@ -0,0 +1,22 @@
+package com.exam;
+
+public class Ticket {
+	String origin;
+	String destination;
+	int price;
+	int quantity;
+
+	public Ticket(String origin, String destination, int price, int quantity) {
+this.origin = origin;
+this.destination = destination;
+this.price = price;
+this.quantity = quantity;
+	}
+	public void print(){
+		System.out.print(origin + "\t" + destination + "\t" + price + "\t" + quantity + "\t" + (price*quantity));
+		
+	}
+}
+	
+		
+
